c92aeaa967abf241b5d5ebf7d2884e45a030e213
Jonas Van Hove <r0741524@student.thomasmore.be>
2023-12-01 15:16:51 +0100

    theming

--- movierecommender/app/src/main/java/com/example/movierecommender/MainActivity.kt
+++ movierecommender/app/src/main/java/com/example/movierecommender/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import com.example.movierecommender.databinding.ActivityMainBinding
 
 import java.util.UUID
@@ -12,6 +15,7 @@ import java.util.UUID
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding;
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +23,15 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         binding.create.setOnClickListener(View.OnClickListener() {
             SocketHandler.setSocket("http://$endpoint")
             SocketHandler.establishConnection()
             val mSocket = SocketHandler.getSocket()
             mSocket?.emit("createRoom", generateID())
-            mSocket?.on("crId") { args->
+            mSocket?.on("crId") { args ->
                 val id = args[0].toString()
                 val b = Bundle()
                 b.putString("rId", id)
@@ -46,7 +53,7 @@ class MainActivity : AppCompatActivity() {
 
     fun generateID(): String {
         val uid = application as UniqueID
-        if (!uid.initialized){
+        if (!uid.initialized) {
             uid.setUniqueId(UUID.randomUUID().toString())
         }
         return uid.uniqueId
