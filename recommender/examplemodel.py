b6993252b6b112418582df51f776f53164361dc7
Joep Demollin <joep.demollin@student.kuleuven.be>
2023-12-07 02:48:17 +0100

    Added api endpoints for simple & full recommendations from movie recommender model

--- recommender/examplemodel.py
+++
@@ -1,6 +0,0 @@
-# LOGIC FOR THE RECOMMENDER MODEL
-filmid = "tt2015381"
-explaination="VinDiesel Space Music" 
-
-def get_recommendation(filmids):
-    return {"filmid":filmid,"img":"test","explanation":explaination}
--- recommender/main.py
+++ recommender/main.py
@@ -1,20 +1,49 @@
 from fastapi import FastAPI
-
-# from typing import Union
-from examplemodel import get_recommendation
+from movie_recommender_model import get_similar_movies, get_explanation
+from models import Movie
 
 app = FastAPI()
 
 
-
 @app.get("/")
-def read_root():
-    return {"Hello":"World"}
+async def read_root():
+    return {"Hello": "World"}
 
 
 @app.post("/submitFilms/{filmids}")
-def submit_films(filmids:str):
+async def submit_films(filmids: str):
     # get some recommendation from model
-    recom = get_recommendation(filmids)
+    recommendation = get_recommendation(filmids)
 
-    return recom
+    return recommendation
+
+
+# TODO: Decide whether films get passed with their title or their index
+@app.get("/simple_recommendation")
+async def get_recommendation(movie: Movie):
+    similar_movies = get_similar_movies(movie.title)
+    recommendations = [
+        {"index": index, "title": title, "overview": overview}
+        for index, (title, overview) in similar_movies[['title', 'overview']].iterrows()
+    ]
+    return {"movie_title": movie.title, "recommendations": recommendations}
+
+
+# The explanation is the intensive part
+@app.get("/full_recommendation")
+async def get_recommendation(movie: Movie):
+    similar_movies = get_similar_movies(movie.title)
+    explanations = get_explanation(similar_movies)
+    recommendations = []
+
+    for index, (title, overview), explanation in zip(similar_movies.index,
+                                                     similar_movies[['title', 'overview']].itertuples(index=False,
+                                                                                                      name=None),
+                                                     explanations):
+        recommendations.append({
+            "index": index,
+            "title": title,
+            "overview": overview,
+            "explanation": explanation
+        })
+    return {"movie_title": movie.title, "recommendations": recommendations}
