5d4c62e021ca71d675e7d846a7089721f0142d4f
Jonas Van Hove <r0741524@student.thomasmore.be>
2023-12-23 22:07:09 +0100

    explanation room ui

--- movierecommender/app/src/main/java/com/example/movierecommender/ExplanationWaitingRoom.kt
+++ movierecommender/app/src/main/java/com/example/movierecommender/ExplanationWaitingRoom.kt
@@ -20,8 +20,8 @@ import kotlin.math.abs
 
 class ExplanationWaitingRoom : AppCompatActivity(), GestureDetector.OnGestureListener {
     private lateinit var binding: ExplanationWaitingRoomBinding
-    private lateinit var roomId:String
-    private lateinit var id:String
+    private lateinit var roomId: String
+    private lateinit var id: String
     private lateinit var mSocket: Socket
 
     private lateinit var gestureDetector: GestureDetector
@@ -37,7 +37,7 @@ class ExplanationWaitingRoom : AppCompatActivity(), GestureDetector.OnGestureLis
         binding = ExplanationWaitingRoomBinding.inflate(layoutInflater)
         gestureDetector = GestureDetector(this, this);
         setContentView(binding.root)
-        mSocket= SocketHandler.getSocket()!!
+        mSocket = SocketHandler.getSocket()!!
         updateLoadingState(true);
 
         val b = intent.extras
@@ -60,7 +60,7 @@ class ExplanationWaitingRoom : AppCompatActivity(), GestureDetector.OnGestureLis
                 val overview = jsonObject.getString("overview")
                 val fullPosterPath = jsonObject.getString("full_poster_path")
                 val explanation = jsonObject.getString("explanation")
-                recommendationBuffer.add( Movie(index,title, overview, fullPosterPath, explanation))
+                recommendationBuffer.add(Movie(index, title, overview, fullPosterPath, explanation))
             }
             updateLoadingState(false)
             displayRecommendation(recommendationBuffer[0]);
@@ -82,6 +82,8 @@ class ExplanationWaitingRoom : AppCompatActivity(), GestureDetector.OnGestureLis
                 binding.tvRecommendationFactors.text = editable
                 editable = Editable.Factory.getInstance().newEditable(recommendationFactors)
                 binding.tvRecommendationExplained.text = editable
+                var recommendationTitle = "Recommendation #" + (currentRecommendationIndex + 1)
+                binding.tvRecommendedMovie.text = recommendationTitle
             }
         }
     }
@@ -110,8 +112,8 @@ class ExplanationWaitingRoom : AppCompatActivity(), GestureDetector.OnGestureLis
         }
     }
 
-    private fun handleNext(){
-        if( currentRecommendationIndex<recommendationBuffer.size-1) {
+    private fun handleNext() {
+        if (currentRecommendationIndex < recommendationBuffer.size - 1) {
             // Display the next recommendation
             val nextRecommendation = recommendationBuffer[++currentRecommendationIndex]
             displayRecommendation(nextRecommendation)
@@ -121,8 +123,8 @@ class ExplanationWaitingRoom : AppCompatActivity(), GestureDetector.OnGestureLis
         }
     }
 
-    private fun handlePrevious(){
-        if( currentRecommendationIndex>0) {
+    private fun handlePrevious() {
+        if (currentRecommendationIndex > 0) {
             // Display the next recommendation
             val nextRecommendation = recommendationBuffer[--currentRecommendationIndex]
             displayRecommendation(nextRecommendation)
@@ -136,8 +138,7 @@ class ExplanationWaitingRoom : AppCompatActivity(), GestureDetector.OnGestureLis
     override fun onTouchEvent(event: MotionEvent): Boolean {
         return if (gestureDetector.onTouchEvent(event)) {
             true
-        }
-        else {
+        } else {
             super.onTouchEvent(event)
         }
     }
@@ -147,7 +148,12 @@ class ExplanationWaitingRoom : AppCompatActivity(), GestureDetector.OnGestureLis
      * It uses the flingThresholds to see whether it was a valid movement.
      * It uses begin and end position to differentiate directions
      */
-    override fun onFling(e1: MotionEvent?, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
         try {
             val diffY = e2.y - e1!!.y
             val diffX = e2.x - e1.x
@@ -157,24 +163,21 @@ class ExplanationWaitingRoom : AppCompatActivity(), GestureDetector.OnGestureLis
                     if (diffX > 0) {
                         // Toast.makeText(applicationContext, "Left to Right swipe gesture", Toast.LENGTH_SHORT).show()
                         handleNext()
-                    }
-                    else {
+                    } else {
                         // Toast.makeText(applicationContext, "Right to Left swipe gesture", Toast.LENGTH_SHORT).show()
                         handlePrevious()
                     }
                 }
-            }else{
+            } else {
                 if (abs(diffY) > flingThreshold && abs(velocityY) > flingVelocityThreshold) {
                     if (diffY > 0) {
                         // Toast.makeText(applicationContext, "Up to Down gesture", Toast.LENGTH_SHORT).show()
-                    }
-                    else {
+                    } else {
                         // Toast.makeText(applicationContext, "Down to Up swipe gesture", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
-        }
-        catch (exception: Exception) {
+        } catch (exception: Exception) {
             exception.printStackTrace()
         }
         return true
@@ -193,7 +196,12 @@ class ExplanationWaitingRoom : AppCompatActivity(), GestureDetector.OnGestureLis
         return false
     }
 
-    override fun onScroll(e1: MotionEvent?, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
         return false
     }
 
